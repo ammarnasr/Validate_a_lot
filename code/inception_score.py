a79e587b02ebdbdce0a1b77b6858cf2d043e555a
ammarnasr <ammarnasraza@gmail.com>
2020-08-10 15:16:00 +0200

    Update inception_score.py

--- code/inception_score.py
+++ code/inception_score.py
@@ -24,7 +24,7 @@ from inception.slim import slim
 import numpy as np
 import tensorflow as tf
 
-
+import pickle
 import math
 import os.path
 import scipy.misc
@@ -43,18 +43,12 @@ else:
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string('checkpoint_dir',
-                           './inception_finetuned_models/birds_valid299/model.ckpt',
-                           """Path where to read model checkpoints.""")
+tf.app.flags.DEFINE_string('checkpoint_dir', './inception_finetuned_models/birds_valid299/model.ckpt', """Path where to read model checkpoints.""")
 
-tf.app.flags.DEFINE_string('image_folder', 
-							'/Users/han/Documents/CUB_200_2011/CUB_200_2011/images',
-							"""Path where to load the images """)
+tf.app.flags.DEFINE_string('image_folder', '/Users/han/Documents/CUB_200_2011/CUB_200_2011/images', """Path where to load the images """)
 
-tf.app.flags.DEFINE_integer('num_classes', 50,      # 20 for flowers
-                            """Number of classes """)
-tf.app.flags.DEFINE_integer('splits', 10,
-                            """Number of splits """)
+tf.app.flags.DEFINE_integer('num_classes', 50, """Number of classes """)
+tf.app.flags.DEFINE_integer('splits', 10,"""Number of splits """)
 tf.app.flags.DEFINE_integer('batch_size', 64, "batch size")
 tf.app.flags.DEFINE_integer('gpu', 0, "The ID of GPU to use")
 # Batch normalization. Constant governing the exponential moving average of
@@ -76,8 +70,7 @@ def preprocess(img):
     # img = Image.fromarray(img, 'RGB')
     if len(img.shape) == 2:
         img = np.resize(img, (img.shape[0], img.shape[1], 3))
-    img = scipy.misc.imresize(img, (299, 299, 3),
-                              interp='bilinear')
+    img = scipy.misc.imresize(img, (299, 299, 3), interp='bilinear')
     img = img.astype(np.float32)
     # [0, 255] --> [0, 1] --> [-1, 1]
     img = img / 127.5 - 1.
@@ -99,7 +92,6 @@ def get_inception_score(sess, images, pred_op):
     indices = list(np.arange(num_examples))
     np.random.shuffle(indices)
     for i in range(n_batches):
-        print ('batch :', i, ' of :', n_batches)
         inp = []
         # print('i*bs', i*bs)
         for j in range(bs):
@@ -125,12 +117,11 @@ def get_inception_score(sess, images, pred_op):
         istart = i * preds.shape[0] // splits
         iend = (i + 1) * preds.shape[0] // splits
         part = preds[istart:iend, :]
-        kl = (part * (np.log(part) -
-              np.log(np.expand_dims(np.mean(part, 0), 0))))
+        kl = (part * (np.log(part) - np.log(np.expand_dims(np.mean(part, 0), 0))))
         kl = np.mean(np.sum(kl, 1))
         scores.append(np.exp(kl))
-    print('mean:', "%.2f" % np.mean(scores), 'std:', "%.2f" % np.std(scores))
     print('################################################################################')
+    print('mean:', "%.2f" % np.mean(scores), 'std:', "%.2f" % np.std(scores))
     print('Mean Inception Score : ' + str((np.mean(scores)).item()) )
     print('################################################################################')
     return np.mean(scores), np.std(scores)
@@ -153,8 +144,7 @@ def load_data(fullpath):
     return images
 
 
-def inference(images, num_classes, for_training=False, restore_logits=True,
-              scope=None):
+def inference(images, num_classes, for_training=False, restore_logits=True, scope=None):
     """Build Inception v3 model architecture.
 
     See here for reference: http://arxiv.org/abs/1512.00567
@@ -163,10 +153,10 @@ def inference(images, num_classes, for_training=False, restore_logits=True,
     images: Images returned from inputs() or distorted_inputs().
     num_classes: number of classes
     for_training: If set to `True`, build the inference model for training.
-      Kernels that operate differently for inference during training
-      e.g. dropout, are appropriately configured.
+        Kernels that operate differently for inference during training
+        e.g. dropout, are appropriately configured.
     restore_logits: whether or not the logits layers should be restored.
-      Useful for fine-tuning a model with different num_classes.
+        Useful for fine-tuning a model with different num_classes.
     scope: optional prefix string identifying the ImageNet tower.
 
     Returns:
@@ -175,10 +165,10 @@ def inference(images, num_classes, for_training=False, restore_logits=True,
     """
     # Parameters for BatchNorm.
     batch_norm_params = {
-      # Decay for the moving averages.
-      'decay': BATCHNORM_MOVING_AVERAGE_DECAY,
-      # epsilon to prevent 0s in variance.
-      'epsilon': 0.001,
+        # Decay for the moving averages.
+        'decay': BATCHNORM_MOVING_AVERAGE_DECAY,
+        # epsilon to prevent 0s in variance.
+        'epsilon': 0.001,
     }
     # Set weight_decay for weights in Conv and FC layers.
     with slim.arg_scope([slim.ops.conv2d, slim.ops.fc], weight_decay=0.00004):
@@ -187,12 +177,12 @@ def inference(images, num_classes, for_training=False, restore_logits=True,
                             activation=tf.nn.relu,
                             batch_norm_params=batch_norm_params):
             logits, endpoints = slim.inception.inception_v3(
-              images,
-              dropout_keep_prob=0.8,
-              num_classes=num_classes,
-              is_training=for_training,
-              restore_logits=restore_logits,
-              scope=scope)
+                images,
+                dropout_keep_prob=0.8,
+                num_classes=num_classes,
+                is_training=for_training,
+                restore_logits=restore_logits,
+                scope=scope)
 
     # Grab the logits associated with the side head. Employed during training.
     auxiliary_logits = endpoints['aux_logits']
@@ -220,9 +210,7 @@ def main(unused_argv=None):
 
                 logits, _ = inference(inputs, num_classes)
                 # calculate softmax after remove 0 which reserve for BG
-                known_logits = \
-                    tf.slice(logits, [0, 1],
-                             [FLAGS.batch_size, num_classes - 1])
+                known_logits = tf.slice(logits, [0, 1], [FLAGS.batch_size, num_classes - 1])
                 pred_op = tf.nn.softmax(known_logits)
 
                 # Restore the moving average version of the
@@ -233,8 +221,37 @@ def main(unused_argv=None):
                 saver = tf.train.Saver(variables_to_restore)
                 saver.restore(sess, FLAGS.checkpoint_dir)
                 print('Restore the model from %s).' % FLAGS.checkpoint_dir)
-                images = load_data(fullpath)
-                get_inception_score(sess, images, pred_op)
+
+
+
+                imagePathList = [
+                    '/content/AttnGAN/models/netG_epoch_0/valid/single/',
+                    '/content/AttnGAN/models/netG_epoch_60/valid/single/',
+                    '/content/AttnGAN/models/netG_epoch_70/valid/single/',
+                    '/content/AttnGAN/models/netG_epoch_80/valid/single/',
+                    '/content/AttnGAN/models/netG_epoch_90/valid/single/',
+                    '/content/AttnGAN/models/netG_epoch_100/valid/single/',
+                    '/content/AttnGAN/models/netG_epoch_110/valid/single/',
+                    '/content/AttnGAN/models/netG_epoch_120/valid/single/',
+                    '/content/AttnGAN/models/netG_epoch_130/valid/single/',
+                    '/content/AttnGAN/models/netG_epoch_140/valid/single/',
+                    '/content/AttnGAN/models/netG_epoch_150/valid/single/',
+                    '/content/AttnGAN/models/netG_epoch_160/valid/single/',
+                ]
+                mean_scores = []
+                std_scores = []
+                for i in imagePathList :
+                    images = load_data(i)
+                    incept_mean, incept_std = get_inception_score(sess, images, pred_op)
+                    mean_scores.append(incept_mean)
+                    std_scores.append(incept_std)
+                    
+                with open('/content/mean_scores.pkl', 'wb') as f:
+                    pickle.dump(mean_scores, f)
+                    
+                with open('/content/std_scores.pkl', 'wb') as f:
+                    pickle.dump(std_scores, f)
+                
 
 
 
